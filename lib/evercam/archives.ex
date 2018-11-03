d3d3ca766b61e2d94b86b3b9f3bbc4c6c3f9b7dd
Junaid Farooq <junaid@evercam.io>
2018-11-03 19:19:53 +0500

    Added get_archive.

--- lib/evercam/archives.ex
+++ lib/evercam/archives.ex
@@ -7,6 +7,12 @@ defmodule Evercam.Archives do
     |> received
   end
 
+  def get_archive(camera_id, archive_id, client) do
+    API.call(client, "/cameras/#{camera_id}/archives/#{archive_id}", :get)
+    |> API.handle_response
+    |> received
+  end
+
   defp received({:error, message, status_code, _headers}) do
     {:error, message, status_code}
   end
